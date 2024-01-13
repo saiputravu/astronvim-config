b283060b5561945c21a990ca50d164baa3221f2e
sai.putravu <sai.putravu@student.manchester.ac.uk>
2024-01-13 17:20:49 +0000

    Initial commit

---
+++ plugins/user.lua
@@ -0,0 +1,41 @@
+return {
+  -- You can also add new plugins here as well:
+  -- Add plugins, the lazy syntax
+  -- "andweeb/presence.nvim",
+  -- {
+  --   "ray-x/lsp_signature.nvim",
+  --   event = "BufRead",
+  --   config = function()
+  --     require("lsp_signature").setup()
+  --   end,
+  -- },
+  {
+    "p00f/clangd_extensions.nvim",
+    init = function()
+      -- load clangd extensions when clangd attaches
+      local augroup = vim.api.nvim_create_augroup("clangd_extensions", { clear = true })
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = augroup,
+        desc = "Load clangd_extensions with clangd",
+        callback = function(args)
+          if assert(vim.lsp.get_client_by_id(args.data.client_id)).name == "clangd" then
+            require "clangd_extensions"
+            -- add more `clangd` setup here as needed such as loading autocmds
+            vim.api.nvim_del_augroup_by_id(augroup) -- delete auto command since it only needs to happen once
+          end
+        end,
+      })
+    end,
+  },
+  {
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    config = true,
+    lazy = false,
+    opts = function(plugin, opts)
+      require("gruvbox").setup({
+        contrast = "hard",
+      })
+    end,
+  },
+}
