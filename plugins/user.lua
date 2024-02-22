9faa983092f8398ed5dd1f552a27aca0fafcdf67
sai.putravu <sai.putravu@student.manchester.ac.uk>
2024-02-22 22:24:48 +0000

    Updates

--- plugins/user.lua
+++ plugins/user.lua
@@ -8,7 +8,68 @@ return {
   --   config = function()
   --     require("lsp_signature").setup()
   --   end,
+  -- },-- lazy.nvim
+  -- {
+  --   "folke/noice.nvim",
+  --   event = "VeryLazy",
+  --   lazy = false,
+  --   opts = function(plugin, ops)
+  --     require("noice").setup({
+  --       lsp = {
+  --         -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+  --         override = {
+  --           ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+  --           ["vim.lsp.util.stylize_markdown"] = true,
+  --           ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+  --           ["config.lsp.hover.enabled"] = false, -- requires hrsh7th/nvim-cmp
+  --         },
+  --       },
+  --       -- you can enable a preset for easier configuration
+  --       presets = {
+  --         bottom_search = true, -- use a classic bottom cmdline for search
+  --         command_palette = true, -- position the cmdline and popupmenu together
+  --         long_message_to_split = true, -- long messages will be sent to a split
+  --         inc_rename = false, -- enables an input dialog for inc-rename.nvim
+  --         lsp_doc_border = false, -- add a border to hover docs and signature help
+  --       },
+  --     })
+  --   end,
+  --   dependencies = {
+  --     -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+  --     "MunifTanjim/nui.nvim",
+  --     -- OPTIONAL:
+  --     --   `nvim-notify` is only needed, if you want to use the notification view.
+  --     --   If not available, we use `mini` as the fallback
+  --     "rcarriga/nvim-notify",
+  --    }
   -- },
+  {
+    "kawre/leetcode.nvim",
+    build = ":TSUpdate html",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "nvim-lua/plenary.nvim", -- required by telescope
+      "MunifTanjim/nui.nvim",
+
+      -- optional
+      "nvim-treesitter/nvim-treesitter",
+      "rcarriga/nvim-notify",
+      "nvim-tree/nvim-web-devicons",
+    },
+    opts = {
+      image_support = false,
+      injector = { ---@type table<lc.lang, lc.inject>
+        ["cpp"] = {
+            before = { "#include <bits/stdc++.h>", "using namespace std;" },
+            after = "int main() {}",
+        },
+        ["java"] = {
+            before = "import java.util.*;",
+        },
+      }-- configuration goes here
+    },
+    lazy = false,
+  },
   {
     "p00f/clangd_extensions.nvim",
     init = function()
